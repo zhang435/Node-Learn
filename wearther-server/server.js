3ce5705ef9fd5059420071188a7c5bff0ca5c25b
Jiawei Zhang <809681148@qq.com>
2017-12-22 12:56:45 -0500

    deloy

--- wearther-server/server.js
+++ wearther-server/server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const hbs     = require("hbs");
+const fs      = require("fs");
+const port    = process.env.PORT || 3000;
 
 var app = express()
 
@@ -8,11 +10,23 @@ app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials")
 hbs.registerHelper("year" , () =>  new Date().getFullYear())
 hbs.registerHelper("screamit", (text) => text.toUpperCase())
+
+app.use((req,res,next) => {
+    var now =  new Date().toString();
+    var content = `${now} : ${req.method} : ${req.url}\n`;
+    fs.appendFile("log",content ,(error) => {})
+    next();
+})
+
+app.use((req,res,next) => {
+    res.render("mantain.hbs");
+})
+
 app.get("/" , (req, res) => {
     // res.send('HELLO EXPRESS');
     res.render("index.hbs",{
         title : "Index",
-        name : "apple",
+        name : "apple", 
         like : ["sleep", "food"],
     })
 })
@@ -24,5 +38,7 @@ app.get("/about" , (req, res) => {
     // res.send('IN the about page');
 
 })
-app.listen(3000); 
+app.listen(port, () => {
+    console.log(`listen a ${port}`);
+}; 
  
